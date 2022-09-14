4e4dfb7207addd9f2e7b8d76886d12863dfe2381
lucaslop <lucaslopes05597@gmail.com>
2022-09-14 11:50:19 -0300

    data file

---
+++ src/data/rotas.ts
@@ -0,0 +1,464 @@
+export const rotas: any = [
+  {
+    "Recanto das Emas:Jardim Botanico": {
+      "distancia": 294,
+      "tempo": 3.675
+    },
+    "Recanto das Emas:Sobradinho II": {
+      "distancia": 356,
+      "tempo": 4.45
+    },
+    "Recanto das Emas:Nucleo Bandeirante": {
+      "distancia": 456,
+      "tempo": 5.45
+    }
+  },
+  {
+    "Taguatinga:Santa Maria": {
+      "distancia": 806,
+      "tempo": 10.075
+    },
+    "Taguatinga:Nucleo Bandeirante": {
+      "distancia": 1283,
+      "tempo": 16.0375
+    },
+    "Taguatinga:Asa Sul": {
+      "distancia": 2941,
+      "tempo": 36.7625
+    },
+    "Taguatinga:Arniqueira": {
+      "distancia": 5298,
+      "tempo": 66.225
+    },
+    "Taguatinga:Paranoa": {
+      "distancia": 2173,
+      "tempo": 27.1625
+    },
+    "Taguatinga:Riacho Fundo 1": {
+      "distancia": 6083,
+      "tempo": 76.0375
+    }
+  },
+  {
+    "Paranoa:Taguatinga": {
+      "distancia": 2173,
+      "tempo": 27.1625
+    }
+  },
+  {
+    "Guara:Varjão": {
+      "distancia": 434,
+      "tempo": 5.425
+    },
+    "Guara:Samambaia": {
+      "distancia": 524,
+      "tempo": 6.55
+    },
+    "Guara:Plano Piloto": {
+      "distancia": 586,
+      "tempo": 7.325
+    },
+    "Guara:Riacho Fundo 2": {
+      "distancia": 1453,
+      "tempo": 18.1625
+    },
+    "Guara:Sobradinho": {
+      "distancia": 906,
+      "tempo": 11.325
+    },
+    "Guara:Brasilia": {
+      "distancia": 716,
+      "tempo": 8.95
+    },
+    "Guara:Sobradinho II": {
+      "distancia": 1372,
+      "tempo": 17.15
+    }
+  },
+  {
+    "Riacho Fundo 1:Arniqueira": {
+      "distancia": 785,
+      "tempo": 9.8125
+    },
+    "Riacho Fundo 1:Taguatinga": {
+      "distancia": 6083,
+      "tempo": 76.0375
+    }
+  },
+  {
+    "Brasilia:Guara": {
+      "distancia": 716,
+      "tempo": 8.95
+    },
+    "Brasilia:Sobradinho": {
+      "distancia": 209,
+      "tempo": 2.6125
+    }
+  },
+  {
+    "Riacho Fundo 2:Asa Sul": {
+      "distancia": 694,
+      "tempo": 8.675
+    },
+    "Riacho Fundo 2:Sobradinho": {
+      "distancia": 934,
+      "tempo": 11.675
+    },
+    "Riacho Fundo 2:Guara": {
+      "distancia": 1453,
+      "tempo": 18.1625
+    },
+    "Riacho Fundo 2:Plano Piloto": {
+      "distancia": 1014,
+      "tempo": 12.675
+    },
+    "Riacho Fundo 2:Gama": {
+      "distancia": 991,
+      "tempo": 12.3875
+    }
+  },
+  {
+    "Asa Sul:Candangolandia": {
+      "distancia": 1456,
+      "tempo": 18.2
+    },
+    "Asa Sul:Arniqueira": {
+      "distancia": 2357,
+      "tempo": 29.4625
+    },
+    "Asa Sul:Taguatinga": {
+      "distancia": 2941,
+      "tempo": 36.7625
+    },
+    "Asa Sul:Nucleo Bandeirante": {
+      "distancia": 1784,
+      "tempo": 22.3
+    },
+    "Asa Sul:Sobradinho": {
+      "distancia": 934,
+      "tempo": 11.675
+    },
+    "Asa Sul:Riacho Fundo 2": {
+      "distancia": 694,
+      "tempo": 8.675
+    }
+  },
+  {
+    "Gama:Plano Piloto": {
+      "distancia": 408,
+      "tempo": 5.1
+    },
+    "Gama:Riacho Fundo 2": {
+      "distancia": 991,
+      "tempo": 12.3875
+    },
+    "Gama:Ceilandia": {
+      "distancia": 300,
+      "tempo": 3.75
+    }
+  },
+  {
+    "Ceilandia:Gama": {
+      "distancia": 300,
+      "tempo": 3.75
+    },
+    "Ceilandia:Park Way": {
+      "distancia": 476,
+      "tempo": 5.95
+    }
+  },
+  {
+    "Asa Norte:Aguas Claras": {
+      "distancia": 634,
+      "tempo": 7.925
+    },
+    "Asa Norte:Fercal": {
+      "distancia": 800,
+      "tempo": 10
+    },
+    "Asa Norte:Vicente Pires": {
+      "distancia": 688,
+      "tempo": 8.6
+    },
+    "Asa Norte:SCIA": {
+      "distancia": 537,
+      "tempo": 6.7125
+    }
+  },
+  {
+    "Sobradinho:Guara": {
+      "distancia": 906,
+      "tempo": 11.325
+    },
+    "Sobradinho:Brasilia": {
+      "distancia": 209,
+      "tempo": 2.6125
+    },
+    "Sobradinho:Riacho Fundo 2": {
+      "distancia": 935,
+      "tempo": 11.6875
+    },
+    "Sobradinho:Asa Sul": {
+      "distancia": 934,
+      "tempo": 11.675
+    },
+    "Sobradinho:Nucleo Bandeirante": {
+      "distancia": 874,
+      "tempo": 10.925
+    },
+    "Sobradinho:Sobradinho II": {
+      "distancia": 1643,
+      "tempo": 20.5375
+    }
+  },
+  {
+    "Vicente Pires:SCIA": {
+      "distancia": 185,
+      "tempo": 2.3125
+    },
+    "Vicente Pires:Aguas Claras": {
+      "distancia": 1224,
+      "tempo": 15.3
+    },
+    "Vicente Pires:Fercal": {
+      "distancia": 120,
+      "tempo": 1.5
+    }
+  },
+  {
+    "Jardim Botanico:Recanto das Emas": {
+      "distancia": 294,
+      "tempo": 3.675
+    },
+    "Jardim Botanico:Fercal": {
+      "distancia": 256,
+      "tempo": 3.2
+    },
+    "Jardim Botanico:Sobradinho II": {
+      "distancia": 120,
+      "tempo": 7.25
+    }
+  },
+  {
+    "Arniqueira:Taguatinga": {
+      "distancia": 5298,
+      "tempo": 66.225
+    },
+    "Arniqueira:Riacho Fundo 1": {
+      "distancia": 785,
+      "tempo": 9.8125
+    },
+    "Arniqueira:Asa Sul": {
+      "distancia": 2357,
+      "tempo": 29.4625
+    },
+    "Arniqueira:Candangolandia": {
+      "distancia": 888,
+      "tempo": 11.1
+    },
+    "Arniqueira:SIA": {
+      "distancia": 1400,
+      "tempo": 17.5
+    }
+  },
+  {
+    "SCIA:Asa Norte": {
+      "distancia": 537,
+      "tempo": 6.7125
+    },
+    "SCIA:Vicente Pires": {
+      "distancia": 185,
+      "tempo": 2.3125
+    }
+  },
+  {
+    "Nucleo Bandeirante:Taguatinga": {
+      "distancia": 1283,
+      "tempo": 16.0375
+    },
+    "Nucleo Bandeirante:Asa Sul": {
+      "distancia": 1784,
+      "tempo": 22.3
+    },
+    "Nucleo Bandeirante:Sobradinho": {
+      "distancia": 874,
+      "tempo": 10.925
+    },
+    "Nucleo Bandeirante:Sobradinho II": {
+      "distancia": 1500,
+      "tempo": 18.75
+    },
+    "Nucleo Bandeirante:Aguas Claras": {
+      "distancia": 1110,
+      "tempo": 2.3125
+    },
+    "Nucleo Bandeirante:Santa Maria": {
+      "distancia": 185,
+      "tempo": 13.875
+    }
+  },
+  {
+    "Park Way:Ceilandia": {
+      "distancia": 476,
+      "tempo": 5.95
+    }
+  },
+  {
+    "Candangolandia:Arniqueira": {
+      "distancia": 901,
+      "tempo": 11.2625
+    },
+    "Candangolandia:Asa Sul": {
+      "distancia": 1456,
+      "tempo": 18.2
+    }
+  },
+  {
+    "Fercal:Asa Norte": {
+      "distancia": 800,
+      "tempo": 10
+    },
+    "Fercal:Aguas Claras": {
+      "distancia": 1137,
+      "tempo": 14.125
+    },
+    "Fercal:Sobradinho II": {
+      "distancia": 839,
+      "tempo": 10.4875
+    },
+    "Fercal:Jardim Botanico": {
+      "distancia": 256,
+      "tempo": 3.2
+    },
+    "Fercal:Vicente Pires": {
+      "distancia": 120,
+      "tempo": 1.5
+    }
+  },
+  {
+    "SIA:Candangolandia": {
+      "distancia": 544,
+      "tempo": 6.8
+    },
+    "SIA:Arniqueira": {
+      "distancia": 1445,
+      "tempo": 18.0625
+    }
+  },
+  {
+    "Varjão:Samambaia": {
+      "distancia": 521,
+      "tempo": 6.5125
+    },
+    "Varjão:Plano Piloto": {
+      "distancia": 429,
+      "tempo": 5.3625
+    },
+    "Varjão:Guara": {
+      "distancia": 1445,
+      "tempo": 5.425
+    }
+  },
+  {
+    "Sobradinho II:Guara": {
+      "distancia": 1372,
+      "tempo": 17.15
+    },
+    "Sobradinho II:Sobradinho": {
+      "distancia": 1643,
+      "tempo": 20.5375
+    },
+    "Nucleo Bandeirante:Sobradinho II": {
+      "distancia": 1500,
+      "tempo": 18.75
+    },
+    "Sobradinho II:Aguas Claras": {
+      "distancia": 1163,
+      "tempo": 14.5375
+    },
+    "Sobradinho II:Samambaia": {
+      "distancia": 1202,
+      "tempo": 15.025
+    },
+    "Recanto das Emas:Sobradinho II": {
+      "distancia": 356,
+      "tempo": 4.45
+    },
+    "Sobradinho II:Jardim Botanico": {
+      "distancia": 632,
+      "tempo": 7.9
+    },
+    "Sobradinho II:Fercal": {
+      "distancia": 839,
+      "tempo": 10.4875
+    }
+  },
+  {
+    "Santa Maria:Taguatinga": {
+      "distancia": 806,
+      "tempo": 10.075
+    },
+    "Santa Maria:Aguas Claras": {
+      "distancia": 446,
+      "tempo": 5.575
+    },
+    "Santa Maria:Nucleo Bandeirante": {
+      "distancia": 1386,
+      "tempo": 17.325
+    }
+  },
+  {
+    "Plano Piloto:Guara": {
+      "distancia": 586,
+      "tempo": 7.325
+    },
+    "Plano Piloto:Riacho Fundo 2": {
+      "distancia": 1014,
+      "tempo": 12.675
+    },
+    "Plano Piloto:Gama": {
+      "distancia": 408,
+      "tempo": 5.1
+    },
+    "Plano Piloto:Varjão": {
+      "distancia": 429,
+      "tempo": 5.3625
+    }
+  },
+  {
+    "Aguas Claras:Asa Norte": {
+      "distancia": 634,
+      "tempo": 7.925
+    },
+    "Aguas Claras:Nucleo Bandeirante": {
+      "distancia": 1401,
+      "tempo": 17.5125
+    },
+    "Aguas Claras:Fercal": {
+      "distancia": 1137,
+      "tempo": 14.125
+    },
+    "Aguas Claras:Sobradinho II": {
+      "distancia": 1163,
+      "tempo": 14.5375
+    },
+    "Aguas Claras:Santa Maria": {
+      "distancia": 446,
+      "tempo": 5.575
+    }
+  },
+  {
+    "Samambaia:Varjão": {
+      "distancia": 521,
+      "tempo": 6.5125
+    },
+    "Samambaia:Sobradinho II": {
+      "distancia": 1202,
+      "tempo": 15.025
+    },
+    "Samambaia:Guara": {
+      "distancia": 524,
+      "tempo": 6.55
+    }
+  }
+]
---
+++ src/data/rotasWH.ts
@@ -0,0 +1,157 @@
+export const rotasWH: any = [
+  {
+    "Recanto das Emas:Jardim Botanico": 294,
+    "Recanto das Emas:Sobradinho II": 356
+  },
+  {
+    "Taguatinga:Santa Maria": 806,
+    "Taguatinga:Nucleo Bandeirante": 1283,
+    "Taguatinga:Asa Sul": 2941,
+    "Taguatinga:Arniqueira": 5298,
+    "Taguatinga:Paranoa": 2173,
+    "Taguatinga:Riacho Fundo 1": 6083
+  },
+  {
+    "Paranoa:Taguatinga": 2173
+  },
+  {
+    "Guara:Varjão": 434,
+    "Guara:Samambaia": 524,
+    "Guara:Plano Piloto": 586,
+    "Guara:Riacho Fundo 2": 1453,
+    "Guara:Sobradinho": 906,
+    "Guara:Brasilia": 716,
+    "Guara:Sobradinho II": 1372
+  },
+  {
+    "Riacho Fundo 1:Arniqueira": 785,
+    "Riacho Fundo 1:Taguatinga": 6083
+  },
+  {
+    "Brasilia:Guara": 716,
+    "Brasilia:Sobradinho": 209
+  },
+  {
+    "Riacho Fundo 2:Asa Sul": 694,
+    "Riacho Fundo 2:Sobradinho": 934,
+    "Riacho Fundo 2:Guara": 1453,
+    "Riacho Fundo 2:Plano Piloto": 1014,
+    "Riacho Fundo 2:Gama": 991
+  },
+  {
+    "Asa Sul:Candangolandia": 1456,
+    "Asa Sul:Arniqueira": 2357,
+    "Asa Sul:Taguatinga": 2941,
+    "Asa Sul:Nucleo Bandeirante": 1784,
+    "Asa Sul:Sobradinho": 934,
+    "Asa Sul:Riacho Fundo 2": 694
+  },
+  {
+    "Gama:Plano Piloto": 408,
+    "Gama:Riacho Fundo 2": 991,
+    "Gama:Ceilandia": 300
+  },
+  {
+    "Ceilandia:Gama": 300,
+    "Ceilandia:Park Way": 476
+  },
+  {
+    "Asa Norte:Aguas Claras": 634,
+    "Asa Norte:Fercal": 800,
+    "Asa Norte:Vicente Pires": 688,
+    "Asa Norte:SCIA": 537
+  },
+  {
+    "Sobradinho:Guara": 906,
+    "Sobradinho:Brasilia": 209,
+    "Sobradinho:Riacho Fundo 2": 935,
+    "Sobradinho:Asa Sul": 934,
+    "Sobradinho:Nucleo Bandeirante": 874,
+    "Sobradinho:Sobradinho II": 1643
+  },
+  {
+    "Vicente Pires:SCIA": 185,
+    "Vicente Pires:Aguas Claras": 1224,
+    "Vicente Pires:Fercal": 120
+  },
+  {
+    "Jardim Botanico:Recanto das Emas": 294,
+    "Jardim Botanico:Fercal": 256,
+    "Jardim Botanico:Sobradinho II": 580
+  },
+  {
+    "Arniqueira:Taguatinga": 5298,
+    "Arniqueira:Riacho Fundo 1": 785,
+    "Arniqueira:Asa Sul": 2357,
+    "Arniqueira:Candangolandia": 888,
+    "Arniqueira:SIA": 1400
+  },
+  {
+    "SCIA:Asa Norte": 537,
+    "SCIA:Vicente Pires": 185
+  },
+  {
+    "Nucleo Bandeirante:Taguatinga": 1283,
+    "Nucleo Bandeirante:Asa Sul": 1784,
+    "Nucleo Bandeirante:Sobradinho": 874,
+    "Nucleo Bandeirante:Sobradinho II": 1500,
+    "Nucleo Bandeirante:Aguas Claras": 1110,
+    "Nucleo Bandeirante:Santa Maria": 1250
+  },
+  {
+    "Park Way:Ceilandia": 476
+  },
+  {
+    "Candangolandia:Arniqueira": 901,
+    "Candangolandia:Asa Sul": 1456
+  },
+  {
+    "Fercal:Asa Norte": 800,
+    "Fercal:Aguas Claras": 1137,
+    "Fercal:Sobradinho II": 839,
+    "Fercal:Jardim Botanico": 285,
+    "Fercal:Vicente Pires": 120
+  },
+  {
+    "SIA:Candangolandia": 544,
+    "SIA:Arniqueira": 1445
+  },
+  {
+    "Varjão:Samambaia": 521,
+    "Varjão:Plano Piloto": 429,
+    "Varjão:Guara": 434
+  },
+  {
+    "Sobradinho II:Guara": 1372,
+    "Sobradinho II:Sobradinho": 1643,
+    "Sobradinho II:Nucleo Bandeirante": 1454,
+    "Sobradinho II:Aguas Claras": 1163,
+    "Sobradinho II:Samambaia": 1202,
+    "Sobradinho II:Recanto das Emas": 356,
+    "Sobradinho II:Jardim Botanico": 632,
+    "Sobradinho II:Fercal": 839
+  },
+  {
+    "Santa Maria:Taguatinga": 806,
+    "Santa Maria:Aguas Claras": 446,
+    "Santa Maria:Nucleo Bandeirante": 1386
+  },
+  {
+    "Plano Piloto:Guara": 586,
+    "Plano Piloto:Riacho Fundo 2": 1014,
+    "Plano Piloto:Gama": 408,
+    "Plano Piloto:Varjão": 429
+  },
+  {
+    "Aguas Claras:Asa Norte": 634,
+    "Aguas Claras:Nucleo Bandeirante": 1401,
+    "Aguas Claras:Fercal": 1137,
+    "Aguas Claras:Sobradinho II": 1163,
+    "Aguas Claras:Santa Maria": 446
+  },
+  {
+    "Samambaia:Varjão": 521,
+    "Samambaia:Sobradinho II": 1202,
+    "Samambaia:Guara": 524
+  }
+]
